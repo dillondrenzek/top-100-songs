5940ac3f789ab16e1df885116585ad994af86634
Dillon Drenzek <dillon@drenzek.com>
2022-12-18 00:06:01 -0800

    local storage

--- src/song-list/song-list.tsx
+++ src/song-list/song-list.tsx
@@ -11,19 +11,100 @@ import {
 import { FormikHelpers } from "formik";
 import KeyboardArrowUpIcon from "@mui/icons-material/KeyboardArrowUp";
 import KeyboardArrowDownIcon from "@mui/icons-material/KeyboardArrowDown";
-import React, { useCallback } from "react";
+import React, { useCallback, useEffect, useState } from "react";
 import { useNewSongForm } from "../use-new-song-form";
-import { NewSong, createSong } from "../song";
+import { NewSong, createSong, Song } from "../song";
 import { useRankedList } from "../use-ranked-list";
 
+enum LocalStorageKey {
+  Songs = "Songs",
+}
+
+function useLocalStorage(key: LocalStorageKey) {
+  const [_value, _setValue] = useState<string | null>(null);
+
+  useEffect(() => {
+    if (localStorage) {
+      _setValue(localStorage.getItem(key));
+    }
+  }, [key]);
+
+  const setValue = useCallback(
+    (value: string) => {
+      try {
+        if (localStorage) {
+          localStorage.setItem(key, value);
+          const stored = localStorage.getItem(key);
+          if (stored === value) {
+            _setValue(stored);
+            return true;
+          }
+        }
+      } catch (e) {
+        console.error("Error setting localstorage:", e);
+      }
+      return false;
+    },
+    [key]
+  );
+
+  return {
+    value: _value,
+    setValue,
+  };
+}
+
+function serializeSongs(songs: Song[]): string {
+  return JSON.stringify(songs);
+}
+
+function deserializeSongs(data: string): Song[] {
+  try {
+    const deserializedData = JSON.parse(data);
+
+    if (Array.isArray(deserializedData)) {
+      return deserializedData;
+    }
+  } catch (e) {
+    console.error(e);
+  }
+  return [];
+}
+
+function useSongs() {
+  const [hasStarted, setHasStarted] = useState(false);
+  const [songs, setSongs] = useState<Song[]>([]);
+  const { value, setValue } = useLocalStorage(LocalStorageKey.Songs);
+
+  // Initialize
+  useEffect(() => {
+    if (!hasStarted) {
+      if (value) {
+        setSongs(deserializeSongs(value));
+      }
+      setHasStarted(true);
+    }
+  }, [hasStarted, value]);
+
+  // When songs changes
+  useEffect(() => {
+    // Set local storage value
+    setValue(serializeSongs(songs));
+  }, [songs, setValue]);
+
+  return {
+    songs,
+    setSongs,
+  };
+}
+
 export function SongList() {
-  const { items, promoteItem, demoteItem, addItem } = useRankedList([
-    { name: "Song Name A", artist: "Artist Name", count: 0 },
-    { name: "Song Name B", artist: "Artist Name", count: 0 },
-    { name: "Song Name C", artist: "Artist Name", count: 0 },
-    { name: "Song Name D", artist: "Artist Name", count: 0 },
-    { name: "Song Name E", artist: "Artist Name", count: 0 },
-  ]);
+  const { songs, setSongs } = useSongs();
+  const { items, promoteItem, demoteItem, addItem } = useRankedList(songs);
+
+  useEffect(() => {
+    setSongs(items);
+  }, [items, setSongs]);
 
   const onSubmit = useCallback(
     (newSong: NewSong, formikHelpers: FormikHelpers<NewSong>) => {
