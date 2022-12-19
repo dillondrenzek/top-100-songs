91bbe93dbda62dce54a849bb1ce082727d3c31c6
Dillon Drenzek <dillon@drenzek.com>
2022-12-18 21:48:41 -0800

    remove unused functions

--- src/song.ts
+++ src/song.ts
@@ -1,9 +1,3 @@
-export const SortSongs = {
-  descending(a: Song, b: Song): number {
-    return b.count - a.count;
-  },
-};
-
 export interface Song {
   id: number;
   count: number;
@@ -12,11 +6,3 @@ export interface Song {
 }
 
 export type NewSong = Pick<Song, "name" | "artist">;
-
-export function createSong(values: NewSong, id: number): Song {
-  return {
-    id,
-    count: 0,
-    ...values,
-  };
-}
