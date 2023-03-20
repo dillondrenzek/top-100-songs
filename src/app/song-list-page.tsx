3b3deaf532ccccdf3b0476c3dda52c071c7b32bc
Dillon Drenzek <dillon@drenzek.com>
2023-03-19 22:32:51 -0700

    remove unused variables

--- src/app/song-list-page.tsx
+++ src/app/song-list-page.tsx
@@ -8,37 +8,27 @@ import {
   Typography,
   Box,
   styled,
-  TextField,
   Menu,
   MenuItem,
   ListItemIcon,
-  Button,
-  Dialog,
-  DialogTitle,
-  DialogContent,
-  DialogContentText,
-  DialogActions,
-} from "@mui/material";
-import { useToggle } from "usehooks-ts";
-import KeyboardArrowUpIcon from "@mui/icons-material/KeyboardArrowUp";
-import KeyboardArrowDownIcon from "@mui/icons-material/KeyboardArrowDown";
-import VerticalAlignTopIcon from "@mui/icons-material/VerticalAlignTop";
-import VerticalAlignBottomIcon from "@mui/icons-material/VerticalAlignBottom";
-import DeleteForeverIcon from "@mui/icons-material/DeleteForever";
-import ArrowForwardIcon from "@mui/icons-material/ArrowForward";
-import ArrowBackwardIcon from "@mui/icons-material/ArrowBack";
-import MoreIcon from "@mui/icons-material/MoreVert";
-import CreateIcon from "@mui/icons-material/Create";
-import React, { ReactNode, useCallback, useEffect, useState } from "react";
-import { NewSongForm } from "./song-list/new-song-form";
-import { useNavigate } from "react-router-dom";
-import { PlayCircleOutline } from "@mui/icons-material";
-import { useLocalStorage } from "usehooks-ts";
-import { LocalStorageKey } from "../lib/local-storage";
-import { AppState, useAppState } from "../use-app-state";
-import { ControlPanel } from "./control-panel";
-import { Song } from "../song";
-import { ListSettings, SettingsModal } from "./song-list-page/settings-modal";
+} from '@mui/material';
+import KeyboardArrowUpIcon from '@mui/icons-material/KeyboardArrowUp';
+import KeyboardArrowDownIcon from '@mui/icons-material/KeyboardArrowDown';
+import VerticalAlignTopIcon from '@mui/icons-material/VerticalAlignTop';
+import VerticalAlignBottomIcon from '@mui/icons-material/VerticalAlignBottom';
+import DeleteForeverIcon from '@mui/icons-material/DeleteForever';
+import ArrowForwardIcon from '@mui/icons-material/ArrowForward';
+import ArrowBackwardIcon from '@mui/icons-material/ArrowBack';
+import MoreIcon from '@mui/icons-material/MoreVert';
+import CreateIcon from '@mui/icons-material/Create';
+import React, { ReactNode, useCallback, useState } from 'react';
+import { NewSongForm } from './song-list/new-song-form';
+import { useNavigate } from 'react-router-dom';
+import { PlayCircleOutline } from '@mui/icons-material';
+import { useAppState } from '../use-app-state';
+import { ControlPanel } from './control-panel';
+import { Song } from '../song';
+import { ListSettings } from './song-list-page/settings-modal';
 
 function ActionsMenuItem(props: {
   onClick?: React.MouseEventHandler<HTMLLIElement>;
@@ -73,20 +63,20 @@ function ActionsMenu(props: {
   return (
     <>
       <IconButton
-        aria-controls={open ? "basic-menu" : undefined}
-        aria-haspopup="true"
-        aria-expanded={open ? "true" : undefined}
+        aria-controls={open ? 'basic-menu' : undefined}
+        aria-haspopup='true'
+        aria-expanded={open ? 'true' : undefined}
         onClick={handleClick}
       >
-        <MoreIcon fontSize="small" />
+        <MoreIcon fontSize='small' />
       </IconButton>
       <Menu
-        id="basic-menu"
+        id='basic-menu'
         anchorEl={anchorEl}
         open={open}
         onClose={handleClose}
         MenuListProps={{
-          "aria-labelledby": "basic-button",
+          'aria-labelledby': 'basic-button',
         }}
       >
         {children}
@@ -106,33 +96,33 @@ function SongListItem(props: {
 
   return (
     <ListItem>
-      <Stack direction="row" alignItems="center" sx={{ flex: "1 0 auto" }}>
+      <Stack direction='row' alignItems='center' sx={{ flex: '1 0 auto' }}>
         <ListItemText
           sx={{
             pr: 1.5,
             mr: 1.5,
-            width: "15px",
-            textAlign: "center",
-            borderRight: "1px solid #aaa",
-            flex: "0 1 auto",
+            width: '15px',
+            textAlign: 'center',
+            borderRight: '1px solid #aaa',
+            flex: '0 1 auto',
           }}
           secondary={rank}
         />
-        <Stack direction="row" alignItems="center" sx={{ mx: 3 }}>
+        <Stack direction='row' alignItems='center' sx={{ mx: 3 }}>
           {iconButtons}
         </Stack>
         <Stack
-          direction="row"
-          justifyContent="space-between"
-          alignItems="center"
+          direction='row'
+          justifyContent='space-between'
+          alignItems='center'
           spacing={1}
-          sx={{ flex: "1 0 auto" }}
+          sx={{ flex: '1 0 auto' }}
         >
-          <Stack direction="row" spacing={2} alignItems="center">
+          <Stack direction='row' spacing={2} alignItems='center'>
             {/* Play Button */}
             <IconButton
               disabled={!song.spotifyId}
-              onClick={() => onPlayClick?.(song?.spotifyId ?? "")}
+              onClick={() => onPlayClick?.(song?.spotifyId ?? '')}
             >
               <PlayCircleOutline />
             </IconButton>
@@ -172,7 +162,7 @@ export function SongListPage() {
   // Chariot - https://open.spotify.com/track/08kTO4EW0jb07zNsCNM83w?si=860c8155a8e4405c
 
   const [playerSpotifyId, setPlayerSpotifyId] = useState(
-    "2H30WL3exSctlDC9GyRbD4"
+    '2H30WL3exSctlDC9GyRbD4'
   );
 
   //
@@ -189,82 +179,82 @@ export function SongListPage() {
     <>
       <Box
         sx={{
-          position: "fixed",
-          height: "100vh",
-          width: "100vw",
+          position: 'fixed',
+          height: '100vh',
+          width: '100vw',
           zIndex: 100,
-          pointerEvents: "none",
+          pointerEvents: 'none',
         }}
       >
         <Card
           sx={{
             px: 1,
             py: 1,
-            position: "absolute",
-            bottom: "0",
-            right: "0",
+            position: 'absolute',
+            bottom: '0',
+            right: '0',
           }}
         >
           <ControlPanel state={state} onSubmit={handleSettingsModalSubmit} />
         </Card>
         <Card
           sx={{
-            display: "flex",
-            position: "absolute",
+            display: 'flex',
+            position: 'absolute',
             bottom: 0,
-            right: "200px",
+            right: '200px',
             borderRadius: 3,
           }}
         >
           <iframe
-            title="Spotify Player"
+            title='Spotify Player'
             src={`https://open.spotify.com/embed/track/${playerSpotifyId}`}
-            width="100%"
-            height="80"
-            frameBorder="0"
-            allow="autoplay; clipboard-write; encrypted-media; fullscreen; picture-in-picture"
-            loading="lazy"
+            width='100%'
+            height='80'
+            frameBorder='0'
+            allow='autoplay; clipboard-write; encrypted-media; fullscreen; picture-in-picture'
+            loading='lazy'
           ></iframe>
         </Card>
       </Box>
       <Stack
-        direction="row"
-        alignItems="flex-start"
+        direction='row'
+        alignItems='flex-start'
         spacing={2}
         paddingX={2}
-        sx={{ height: "100vh", width: "100%", position: "fixed" }}
+        sx={{ height: '100vh', width: '100%', position: 'fixed' }}
       >
         <Stack
-          direction="column"
-          alignItems="stretch"
+          direction='column'
+          alignItems='stretch'
           spacing={2}
           sx={{
-            height: "100%",
-            width: "50%",
-            maxWidth: "1200px",
+            height: '100%',
+            width: '50%',
+            maxWidth: '1200px',
           }}
         >
           <Stack
             spacing={3}
-            direction="row"
-            sx={{ alignItems: "center", flex: "0 0 auto" }}
+            direction='row'
+            sx={{ alignItems: 'center', flex: '0 0 auto' }}
           >
-            <Typography variant="h4">Main</Typography>
-            <Card sx={{ px: 3, py: 2, flex: "1 0 auto" }}>
+            <Typography variant='h4'>Main</Typography>
+            <Card sx={{ px: 3, py: 2, flex: '1 0 auto' }}>
               <NewSongForm
                 allSongs={top.songs}
-                onSubmit={(newSong) => createSongInList(newSong, "topSongs")}
+                onSubmit={(newSong) => createSongInList(newSong, 'topSongs')}
               />
             </Card>
           </Stack>
-          <Box sx={{ flex: "1 1 auto", overflow: "auto" }}>
+          <Box sx={{ flex: '1 1 auto', overflow: 'auto' }}>
             <Card sx={{ px: 1, py: 1 }}>
               <Stack
                 spacing={2}
-                direction="row"
-                sx={{ alignItems: "center", justifyContent: "center" }}
+                direction='row'
+                sx={{ alignItems: 'center', justifyContent: 'center' }}
               >
-                <Typography textAlign="center" variant="subtitle2">
+                <Typography textAlign='center' variant='subtitle2'>
                   {top.songs.length}/{state.maxTopSongs} songs
                 </Typography>
               </Stack>
@@ -299,21 +289,21 @@ export function SongListPage() {
                           <ActionsMenuItem
                             icon={<CreateIcon />}
                             onClick={() => navigate(`/songs/${song.id}`)}
-                            text={"Edit"}
+                            text={'Edit'}
                           />
                           {/* Move To Bubble */}
                           <ActionsMenuItem
                             icon={<ArrowForwardIcon />}
                             onClick={() =>
-                              moveSongFromList(song, "topSongs", "bubble")
+                              moveSongFromList(song, 'topSongs', 'bubble')
                             }
-                            text={"Move to Bubble"}
+                            text={'Move to Bubble'}
                           />
                           {/* Delete */}
                           <ActionsMenuItem
                             icon={<DeleteForeverIcon />}
                             onClick={() => top.removeSong(song)}
-                            text={"Delete"}
+                            text={'Delete'}
                           />
                         </ActionsMenu>
                       }
@@ -323,11 +313,11 @@ export function SongListPage() {
               ) : (
                 <Box
                   sx={{
-                    display: "flex",
-                    justifyContent: "center",
-                    alignItems: "center",
-                    height: "250px",
-                    backgroundColor: "#eaeaea",
+                    display: 'flex',
+                    justifyContent: 'center',
+                    alignItems: 'center',
+                    height: '250px',
+                    backgroundColor: '#eaeaea',
                   }}
                 >
                   <Typography>No List</Typography>
@@ -337,28 +327,28 @@ export function SongListPage() {
           </Box>
         </Stack>
         <Stack
-          direction="column"
+          direction='column'
           spacing={2}
           marginY={2}
           sx={{
-            height: "100%",
-            overflow: "auto",
-            width: "50%",
-            maxWidth: "1200px",
+            height: '100%',
+            overflow: 'auto',
+            width: '50%',
+            maxWidth: '1200px',
           }}
         >
-          <Stack spacing={3} direction="row" sx={{ alignItems: "center" }}>
-            <Typography variant="h4">Bubble</Typography>
-            <Card sx={{ px: 3, py: 2, flex: "1 0 auto" }}>
+          <Stack spacing={3} direction='row' sx={{ alignItems: 'center' }}>
+            <Typography variant='h4'>Bubble</Typography>
+            <Card sx={{ px: 3, py: 2, flex: '1 0 auto' }}>
               <NewSongForm
                 allSongs={bubble.songs}
-                onSubmit={(newSong) => createSongInList(newSong, "bubble")}
+                onSubmit={(newSong) => createSongInList(newSong, 'bubble')}
               />
             </Card>
           </Stack>
-          <Box sx={{ flex: "1 0 auto", overflow: "auto" }}>
+          <Box sx={{ flex: '1 0 auto', overflow: 'auto' }}>
             <Card sx={{ px: 1, py: 1 }}>
-              <Typography textAlign="center" variant="subtitle2">
+              <Typography textAlign='center' variant='subtitle2'>
                 {bubble.songs.length} songs
               </Typography>
               {bubble.songs.length ? (
@@ -394,21 +384,21 @@ export function SongListPage() {
                           <ActionsMenuItem
                             icon={<CreateIcon />}
                             onClick={() => navigate(`/songs/${song.id}`)}
-                            text={"Edit"}
+                            text={'Edit'}
                           />
                           {/* Move To Main */}
                           <ActionsMenuItem
                             icon={<ArrowBackwardIcon />}
                             onClick={() =>
-                              moveSongFromList(song, "bubble", "topSongs")
+                              moveSongFromList(song, 'bubble', 'topSongs')
                             }
-                            text={"Move to Main"}
+                            text={'Move to Main'}
                           />
                           {/* Delete */}
                           <ActionsMenuItem
                             icon={<DeleteForeverIcon />}
                             onClick={() => bubble.removeSong(song)}
-                            text={"Delete"}
+                            text={'Delete'}
                           />
                         </ActionsMenu>
                       }
@@ -418,11 +408,11 @@ export function SongListPage() {
               ) : (
                 <Box
                   sx={{
-                    display: "flex",
-                    justifyContent: "center",
-                    alignItems: "center",
-                    height: "250px",
-                    backgroundColor: "#eaeaea",
+                    display: 'flex',
+                    justifyContent: 'center',
+                    alignItems: 'center',
+                    height: '250px',
+                    backgroundColor: '#eaeaea',
                   }}
                 >
                   <Typography>No List</Typography>
